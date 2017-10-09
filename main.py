843b0988711813365ebf9f2ccb3507469a9758c6
Jesse Corrington <jesse.corrington@gmail.com>
2017-10-08 20:41:27 -0700

    testing current code

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from database import *
 if __name__ == '__main__':
     coins = get_coin_list()
 
-    #subreddit = get_subreddit(coins["link"]["id"])
-    #print(subreddit)
+    subreddit = get_subreddit(coins["link"]["id"])
+    print(subreddit)
 
-    #prices = get_historical_prices(coins["link"]["id"])
-    #print(prices)
+    prices = get_historical_prices(coins["link"]["id"])
+    print(prices)
 
-    #stats = get_current_stats("bitcoin")
-    #print(stats)
+    stats = get_current_stats("bitcoin")
+    print(stats)
 
-    #stats = get_historical_stats("loopring")
-    #print(stats)
+    stats = get_historical_stats("loopring")
+    print(stats)
