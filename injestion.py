a82465320c5d67a723ed119a175b4c35b96435f5
Jesse Corrington <jesse.corrington@gmail.com>
2017-11-02 10:43:54 -0700

    coin list injestion now working

--- injestion.py
+++ injestion.py
@@ -4,25 +4,34 @@ import database as db
 from util import timestamp
 
 
-# TODO: periodically we need to make sure the reddit's haven't changed, so do a scan all
+# TODO: periodically we need to make sure the reddit's haven't changed
+# add a param that will just force a full update of all and run every few days
 def update_coin_list():
     print("Updating coins list")
+
+    start_time = timestamp()
 
     current_coins = get_coin_list()
 
     stored_coins = db.get_coins()
     stored_symbols = set()
     added_symbols = set()
-    missing_subreddit = set()
+    missing_subreddits = set()
+    new_symbols = set()
 
     for coin in stored_coins:
         stored_symbols.add(coin["symbol"])
 
     for symbol in current_coins:
-        if symbol in stored_symbols:
-            # We already have the data about this coin
-            continue
+        if symbol not in stored_symbols:
+            new_symbols.add(symbol)
 
+    print("Total current coins (coinmarketcap.com):", len(current_coins))
+    print("Locally stored coins:", len(stored_symbols))
+    print("New coins to process:", len(new_symbols))
+
+    processed = 0
+    for symbol in new_symbols:
         coin = current_coins[symbol]
         cmc_id = coin["cmc_id"]
 
@@ -31,16 +40,23 @@ def update_coin_list():
         except Exception as err:
             # TODO: try looking it up on cryptocompare too, maybe that should be our first source of data
             print("Error getting subreddit: ", err)
-            missing_subreddit.add(symbol)
+            missing_subreddits.add(symbol)
 
         db.insert_coin(coin)
         added_symbols.add(symbol)
         print("Added coin", symbol)
 
-    print("Updatting coin list completed")
+        processed += 1
+
+        print("Progress", processed, "/", len(new_symbols))
+
+    end_time = timestamp()
+    elapsed_time = (end_time - start_time) / 1000
+
+    print("Updating coin list completed in", elapsed_time, "seconds")
     print("Total coins", len(current_coins))
     print("Added", len(added_symbols), "new coins")
-    print("Missing subreddits", len(missing_subreddit))
+    print("Missing subreddits", len(missing_subreddits), missing_subreddits)
 
     # TODO: errors
 
