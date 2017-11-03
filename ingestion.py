1f2b2831ebca9aecb29216f41b52829c04d9786c
Jesse Corrington <jesse.corrington@gmail.com>
2017-11-02 17:44:31 -0700

    start of ingestion task runner architecture, to make adding tasks easier

--- ingestion.py
+++ ingestion.py
@@ -8,10 +8,6 @@ from datetime import date
 # TODO: periodically we need to make sure the reddit's haven't changed
 # add a param that will just force a full update of all and run every few days
 def update_coin_list():
-    print("Updating coin list")
-
-    start_time = timestamp()
-
     current_coins = get_coin_list()
 
     stored_coins = db.get_coins()
@@ -50,11 +46,6 @@ def update_coin_list():
         processed += 1
         print("Progress", processed, "/", len(new_symbols))
 
-    end_time = timestamp()
-    elapsed_time = (end_time - start_time) / 1000
-
-    print("Updating coin list completed in", elapsed_time, "seconds")
-
     if len(new_symbols) > 0:
         print("Total coins", len(current_coins))
         print("Added", len(added_symbols), "new coins")
@@ -69,7 +60,7 @@ def update_coin_list():
 
 def _outdated_historic(coins, latest_updates):
     coins_to_update = {}
-    # Make a list of coins that don't have up to date historic prices stored
+    # Make a list of coins that don't have up to date historic data
     for symbol in coins:
         update_start = datetime.date(2011, 1, 1)
 
@@ -87,82 +78,43 @@ def _outdated_historic(coins, latest_updates):
     return coins_to_update
 
 
-def _ingest_historic(symbol, get_data):
-    # does: timing, progress, what needs updating, writting to ingestion_runs table, error tracking, loggin
+def _ingest_historic(name, get_latest_saved, get_new_data, insert_data):
+    # TODO: how to do timing in clean generic way
+    # TODO: write a record to the injestion collection
 
-    return None
+    coins = db.get_coins()
+    latest_data = get_latest_saved()
+    print("Coins with no", name, "data", len(coins) - len(latest_data))
 
-def _ingest_current(symbol, get_data):
-    # Should we able to handle doing all symbols at once too, ie: cmc price ticker
+    coins_to_update = _outdated_historic(coins, latest_data)
+    print("Coins with out of date", name, "data:", len(coins_to_update))
+    processed = 0
 
-    return None
+    for symbol in coins_to_update:
+        coin = coins[symbol]
+        update_start = coins_to_update[symbol]
+
+        new_data = get_new_data(coin["cmc_id"], start=update_start)
+        if new_data:
+            for day in new_data:
+                day["symbol"] = coin["symbol"]
+
+            insert_data(new_data)
+
+            print("Added all historic", name, "data for", coin["symbol"])
+        else:
+            print("Error: no historic data found for", symbol, "starting on", update_start)
+
+        processed += 1
+        print("Progress", processed, "/", len(coins_to_update))
 
 
 def save_historic_prices():
-    print("Updating historic prices")
-
-    coins = db.get_coins()
-    latest_prices = db.get_latest_prices()
-    print("Coins with no price data", len(coins) - len(latest_prices))
-
-    test = latest_prices["BTC"]
-
-    coins_to_update = _outdated_historic(coins, latest_prices)
-    print("Coins with out of date price data:", len(coins_to_update))
-    processed = 0
-
-    # TODO: this is slightly broken, because it will continue to think we have missing data for coins
-    # that are dead (https://coinmarketcap.com/currencies/paypeer/)
-    # or maybe they don't always get updated on cmc
-    for symbol in coins_to_update:
-        coin = coins[symbol]
-        start_time = coins_to_update[symbol]
-
-        all_prices = get_historical_prices(coin["cmc_id"], start=start_time)
-
-        if all_prices:
-            for day in all_prices:
-                day["symbol"] = coin["symbol"]
-
-            db.insert_prices(all_prices)
-
-            print("added all prices for", coin["symbol"])
-        else:
-            print("Error: no price data found", symbol, start_time)
-
-        processed += 1
-        print("Progress", processed, "/", len(coins_to_update))
+    _ingest_historic("Prices", db.get_latest_prices, get_historical_prices, db.insert_prices)
 
 
 def save_historic_reddit_stats():
-    print("Updating historic reddit stats")
-
-    coins = db.get_coins_with_subreddits()
-    print(len(coins), "Coins with subreddits")
-
-    latest_stats = db.get_latest_social_stats()
-    print("Coins with no social stats saved", len(coins) - len(latest_stats))
-
-    coins_to_update = _outdated_historic(coins, latest_stats)
-    print("Coins with out of date social stats:", len(coins_to_update))
-    processed = 0
-
-    for symbol in coins_to_update:
-        coin = coins[symbol]
-        start_time = coins_to_update[symbol]
-
-        # TODO: need to be able to pass in start time here
-        all_stats = get_historical_stats(coin["subreddit"], coin["symbol"])
-
-        if all_stats:
-            db.insert_social_stats(all_stats)
-            print("Added all historic reddit stats for", coin["symbol"])
-        else:
-            print("Error: no reddit stats for", symbol)
-
-        processed += 1
-        print("Progress", processed, "/", len(coins_to_update))
-
+    _ingest_historic("Reddit stats", db.get_latest_social_stats, get_historical_stats, db.insert_social_stats)
 
 
 def run_all():
@@ -172,14 +124,24 @@ def run_all():
     db.create_indexes()
 
     # TODO: do the timing here
-    update_coin_list()
-    save_historic_prices()
-    save_historic_reddit_stats()
+    tasks = {
+        "Update coin list": update_coin_list,
+        "Update historic prices": save_historic_prices,
+        "Update historic reddit stats": save_historic_reddit_stats
+    }
 
-    end_time = timestamp()
-    elapsed_time = end_time - start_time
+    for name, task in tasks.items():
+        task_start = timestamp()
+        print("Running ingestion task:", name)
 
-    # TODO: write a row to ingestion table (start time, end time, elapsed time, errors, processed, list of import fns run)
+        # TODO: write error and warning count to db
+        errors = task()
+        task_elapsed = timestamp() - task_start
+
+        print("Ingestion task", name, "completed in", task_elapsed / 1000, "seconds")
+        # TODO: write a row to ingestion table (start time, end time, elapsed time, errors, processed, list of import fns run)
+
+    elapsed_time = timestamp() - start_time
 
     print("Ingestion complete, elapsed time (ms):", elapsed_time)
 
