fe90f069dc4422e893def773ef5be95206e3a1ff
Jesse Corrington <jesse.corrington@gmail.com>
2017-11-05 16:46:48 -0800

    small tweak to get things working on python 3.5

--- ingestion/util.py
+++ ingestion/util.py
@@ -6,12 +6,12 @@ import time
 
 def geturl_json(url):
     ret = urllib.request.urlopen(url)
-    return json.loads(ret.read())
+    return json.loads(ret.read().decode("utf-8"))
 
 
 def geturl_text(url):
     ret = urllib.request.urlopen(url)
-    return ret.read().decode()
+    return ret.read().decode("utf-8")
 
 
 # TODO: get rid of this and use datatime objects everything for consistency
