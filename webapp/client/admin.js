5d9b70c41b649bc7ad26eb7beb58858138606e92
Jesse Corrington <jesse.corrington@gmail.com>
2017-11-09 19:19:04 -0800

    add charts to see errors and warnings over time on the admin page

--- webapp/client/admin.js
+++ webapp/client/admin.js
@@ -82,10 +82,38 @@ function updateRunningTasks() {
     });
 }
 
+
 function updateCompletedTasks() {
     $.getJSON('/api/ingestion_tasks?running=false', function (tasks) {
         processTasks(tasks);
-        completedTasksTable.setData(tasks)
+        completedTasksTable.setData(tasks);
+
+        taskNames = {}
+        tasks.forEach(function(task) {
+            taskNames[task.name] = 1;
+        });
+
+        var select = $("#taskSelect");
+        for (name in taskNames) {
+            var op = $("<option>", {value: name});
+            op.html(name);
+
+            select.append(op);
+        }
+
+        function showFor(name) {
+            var coinlistTasks = tasks.filter(function(task){
+                return task.name == name;
+            });
+            buildChart(name, coinlistTasks);
+        }
+
+        select.change(function(val) {
+            name = select.val();
+            showFor(name);
+        });
+
+        showFor("ImportCoinList")
     });
 }
 
@@ -95,4 +123,73 @@ function updateTasks() {
     setInterval(updateRunningTasks, 5000);
 
     updateCompletedTasks();
+}
+
+
+
+function buildChart(name, tasks) {
+    function toSeries(key) {
+        points = [];
+        tasks.forEach(function (task) {
+            points.push([task.start_time, task[key]]);
+        });
+
+        return points;
+    }
+
+    errors = toSeries("errors");
+    httpErrors = toSeries("errors_http");
+    warnings = toSeries("warnings");
+
+
+    Highcharts.chart('chart', {
+    chart: {
+        type: 'spline'
+    },
+    title: {
+        text: 'Errors over time'
+    },
+    subtitle: {
+        text: 'task ' + name
+    },
+    xAxis: {
+        type: 'datetime',
+        dateTimeLabelFormats: { // don't display the dummy year
+            month: '%e. %b',
+            year: '%b'
+        },
+        title: {
+            text: 'Date'
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Count'
+        },
+        min: 0
+    },
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br>',
+        pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
+    },
+
+    plotOptions: {
+        spline: {
+            marker: {
+                enabled: true
+            }
+        }
+    },
+
+    series: [{
+            name: "Errors",
+            data: errors
+        }, {
+            name: "HTTP Errors",
+            data: httpErrors
+        }, {
+            name: "Warnings",
+            data: warnings
+        }]
+});
 }
