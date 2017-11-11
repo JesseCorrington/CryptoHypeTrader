e99aeac6553567a2762106b09ced16d657aeb1e8
Jesse Corrington <jesse.corrington@gmail.com>
2017-11-10 17:23:41 -0800

    handle cmd line args to switch beteween historic and current imports

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 from ingestion import tasks
 
+def getopts(argv):
+    opts = {}
+    while argv:
+        if argv[0][0] == '-':
+            opts[argv[0]] = argv[1]
+        argv = argv[1:]
+    return opts
+
+
+def main():
+    from sys import argv
+    opts = getopts(argv)
+    if "-m" in opts:
+        run_mode = opts["-m"]
+    else:
+        print("Error: must specificy -mode")
+        return
+
+    if run_mode == "historic":
+        tasks.import_historic_data()
+    elif run_mode == "current":
+        tasks.import_current_data()
+    else:
+        print("Error: invalid run mode", run_mode)
+
+
 if __name__ == "__main__":
-    tasks.import_historic_data()
+    main()
