95e964ed267f3b49de7dd6154cffca6d2eec1353
Jesse Corrington <jesse.corrington@gmail.com>
2017-11-11 14:20:07 -0800

    some basic json endpoints for testing

--- webapp/server/dev_server.py
+++ webapp/server/dev_server.py
@@ -1,11 +1,12 @@
 import flask
 import os
 import json
+import pymongo
+from webapp.server import config
 from bson import ObjectId
 from datetime import datetime
 
 # TODO: prob want to move database out of ingestion module, maybe
-from ingestion import database as db
 
 # This is just a basic dev server for easy internal testing
 # Basic REST API access is provided to the database with Eve,
@@ -14,37 +15,54 @@ from ingestion import database as db
 cwd = os.path.dirname(os.path.realpath(__file__))
 public_dir = cwd + "/../client"
 
-domain = {
-    'coins': {},
-    'prices': {},
-    'ingestion_tasks': {},
-    'social_stats': {},
-}
-
-settings = {
-    'MONGO_HOST': 'localhost',
-    'MONGO_PORT': 27017,
-    'MONGO_DBNAME': 'hype-db',
-    'DOMAIN': domain,
-    'ALLOW_UNKNOWN': True,
-    'URL_PREFIX': "api",
-    'PAGINATION': False
-}
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
         if isinstance(o, datetime):
-            return o.timestamp() * 1000
+            return int(o.timestamp() * 1000)
 
         return json.JSONEncoder.default(self, o)
 
 
+def json_response(items, key_filter=None):
+    if not isinstance(items, list):
+        items = list(items)
 
+    if key_filter == None:
+        return JSONEncoder().encode(items)
+
+    filtered = []
+    for item in items:
+        new = {}
+        filtered.append(new)
+        for key in key_filter:
+            new[key] = item[key]
+
+    return JSONEncoder().encode(filtered)
 
 
 app = flask.Flask(__name__)
+
+username = None
+password = None
+if "username" in config.database and "password" in config.database:
+    username = config.database["username"]
+    password = config.database["password"]
+
+
+MONGO_CLIENT = pymongo.MongoClient(
+    config.database["host"],
+    config.database["port"],
+    username=username,
+    password=password,
+    authSource='hype-db',
+    authMechanism='SCRAM-SHA-1',
+    serverSelectionTimeoutMS=3)
+
+MONGO_DB = MONGO_CLIENT[config.database["name"]]
+
 
 @app.route('/<path:path>')
 def static_proxy(path):
@@ -62,9 +80,36 @@ def get_tasks():
     if running:
         query["running"] = to_bool(running)
 
-    items = db.MONGO_DB.ingestion_tasks.find(query)
-    l = list(items)
+    tasks = MONGO_DB.ingestion_tasks.find(query)
+    return json_response(tasks)
 
-    return JSONEncoder().encode(l)
+
+@app.route('/api/coins')
+def get_coins():
+    coins = MONGO_DB.coins.find()
+    return json_response(coins)
+
+
+@app.route('/api/historic_prices')
+def get_historic_prices():
+    coin_id = 1
+    prices = list(MONGO_DB.historic_prices.find({"coin_id": coin_id}))
+
+    # TODO: need ways to get candle data too
+
+    ret = []
+    for price in prices:
+        ret. append([price["date"], price["close"]])
+
+    return JSONEncoder().encode(ret)
+
+
+@app.route('/api/historic_social_stats')
+def get_historic_social_stats():
+    coin_id = 1
+    stats = MONGO_DB.historic_social_stats.find({"coin_id": coin_id})
+
+    return json_response(stats, {"date", "reddit_subscribers"})
+
 
 app.run()
