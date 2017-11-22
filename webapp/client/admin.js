55c39353ea1dc49b7ecd9fda2c349b75f80d0dce
Jesse Corrington <jesse.corrington@gmail.com>
2017-11-21 17:36:46 -0800

    new vuetify table mostly working

--- webapp/client/admin.js
+++ webapp/client/admin.js
@@ -1,123 +1,36 @@
 "use strict"
 
 
+Vue.use(Vuetify)
 
-
-
-function list_count(list) {
-    if (list) return list.length
-    return 0
-}
-
-var DISPLAY_FORMATS = {
-    start_time: 'DateTime',
-    end_time: 'DateTime',
-    percent_done: '(0.00 %)',
-    errors: list_count,
-    errors_http: list_count,
-    warnings: list_count
-}
-
-var taskView = undefined;
-
-
-class RunningTaskTable extends Table {
-    constructor(cointainer, width, height) {
-        super(cointainer, width, height);
-
-        super.setColumns([
-            // TODO: we could set cols in the ctor
-            {header: "Name", key: "name"},
-            {header: "Start Time", key: "start_time"},
-            {header: "Elapsed", key: "elapsed_time"},
-            {header: "Errors", key: "errors"},
-            {header: "HTTP Errors", key: "errors_http"},
-            {header: "Warnings", key: "warnings"},
-            {header: "% Complete", key: "percent_done"},
-            {header: "HTTP requests", key: "http_requests"},
-            {header: "DB inserts", key: "db_inserts"},
-        ]);
-    }
-
-    setData(stats) {
-        super.setData(stats, DISPLAY_FORMATS);
-    }
-}
-
-class CompletedTaskTable extends Table {
-    constructor(cointainer, width, height) {
-        super(cointainer, width, height);
-
-        super.setColumns([
-            // TODO: we could set cols in the ctor
-            {header: "Name", key: "name"},
-            {header: "Start Time", key: "start_time"},
-            {header: "Elapsed", key: "elapsed_time"},
-            {header: "Failed", key: "failed"},
-            {header: "Canceled", key: "canceled"},
-            {header: "Errors", key: "errors"},
-            {header: "HTTP Errors", key: "errors_http"},
-            {header: "Warnings", key: "warnings"},
-            {header: "% Complete", key: "percent_done"},
-            {header: "HTTP requests", key: "http_requests"},
-            {header: "DB inserts", key: "db_inserts"},
-        ]);
-    }
-
-    setData(stats) {
-        super.setData(stats, DISPLAY_FORMATS);
-    }
-}
-
-var runningTaksTable = undefined;
-var completedTasksTable = undefined;
-
-var app = undefined;
-function createComponents() {
-    Vue.use(Vuetify)
-
-    app = new Vue({
-      el: '#vueApp',
-      data: {
-
-          // TODO: what's the proper way to deal with lazy loading?
-          tableDataSimple: [],
-          selected: {},
-          headers: [
-          { text: 'Name', value: 'name'},
+var app = new Vue({
+  el: '#vueApp',
+  data: {
+      // TODO: what's the proper way to deal with lazy loading?
+      headers: [
+          { text: 'Name', value: 'name', align: "left"},
           { text: 'Start Time', value: 'start_time' },
+          { text: 'End Time', value: 'end_time' },
           { text: 'Running', value: 'running' },
           { text: 'Failed', value: 'failed' },
           { text: 'Canceled', value: 'canceled' },
-
+          { text: 'Errors', value: 'errors' },
+          { text: 'Warnings', value: 'warnings' },
+          { text: 'HTTP Errors', value: 'errors_http' },
         ],
-        items: [],
-        search: "",
-      },
+      items: [],
+      selected: [],
+      search: ""
+  },
 
-      mounted: function() {
-          var self = this
-          $.getJSON('/api/ingestion_tasks?running=false', function (json) {
-              self.tableDataSimple = json;
-              self.selected = self.tableDataSimple[0]
-              self.items = json;
-
-          });
-      }
-    });
-}
-
-
-$(document).ready(function () {
-
-    createComponents()
-
-
-    /*runningTaksTable = new RunningTaskTable("runningTasksTable")
-    completedTasksTable = new CompletedTaskTable("completedTasksTable")
-
-    updateTasks()*/
+  mounted: function() {
+      var self = this
+      $.getJSON('/api/ingestion_tasks', function (json) {
+          self.items = json;
+      });
+  }
 });
+
 
 function processTasks(tasks) {
     tasks.forEach(
@@ -129,64 +42,6 @@ function processTasks(tasks) {
             task.elapsed_time = Math.round(task.elapsed_time / 1000);
     });
 }
-
-
-function updateRunningTasks() {
-    $.getJSON('/api/ingestion_tasks?running=true', function (tasks) {
-        processTasks(tasks);
-        runningTaksTable.setData(tasks);
-    });
-}
-
-
-function updateCompletedTasks() {
-    $.getJSON('/api/ingestion_tasks?running=false', function (tasks) {
-        processTasks(tasks);
-        completedTasksTable.setData(tasks);
-
-        var taskNames = {}
-        tasks.forEach(function(task) {
-            taskNames[task.name] = 1;
-        });
-
-        var select = $("#taskSelect");
-        for (var name in taskNames) {
-            var op = $("<option>", {value: name});
-            op.html(name);
-
-            select.append(op);
-        }
-
-        function showFor(name) {
-            var coinlistTasks = tasks.filter(function(task){
-                return task.name == name;
-            });
-            buildChart(name, coinlistTasks);
-        }
-
-        select.change(function(val) {
-            name = select.val();
-            showFor(name);
-        });
-
-        showFor("ImportCoinList")
-
-        taskView = new TaskView(tasks[0])
-        taskView.render()
-
-        var parent = $("#taskDetails");
-        parent.append(taskView._container)
-    });
-}
-
-
-function updateTasks() {
-    updateRunningTasks();
-    //setInterval(updateRunningTasks, 5000);
-
-    updateCompletedTasks();
-}
-
 
 
 
@@ -256,48 +111,4 @@ function buildChart(name, tasks) {
             data: warnings
         }]
 });
-}
-
-
-
-class TaskView extends Component {
-    constructor(task) {
-        super();
-        this._task = task;
-    }
-
-    render() {
-
-
-
-        var self = this
-        function line(name, key) {
-            return `<br/>${name}: ${self._task[key]}`
-        }
-
-        var html = line("Name", "name");
-        html += line("Start Time", "start_time");
-        html += line("Elapsed Time", "elapsed_time");
-
-        function error_list(name, errors) {
-            html = `<hX>${name}(${errors.length})</hX>`
-            html += "<ul>"
-
-            errors.forEach(function(error) {
-                html += "<li>"
-                html += error
-                html += "</li>"
-            });
-
-            html += "</ul>"
-            return html
-        }
-
-        html += "<br/>"
-        html += error_list("Errors", this._task.errors);
-        html += error_list("Warnings", this._task.warnings);
-        html += error_list("HTTP Errors", this._task.errors_http);
-
-        this._container.append(html);
-    }
 }
