716e9c770f6bfba2ec9ffa7f82f566b08817d649
Jesse Corrington <jesse.corrington@gmail.com>
2018-06-10 23:09:25 -0700

    more functional task list

--- webapp/client/src/components/Admin.vue
+++ webapp/client/src/components/Admin.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+      <h1>Ingestion Tasks</h1>
       <v-card>
       <v-data-table
           :headers="headers"
@@ -15,6 +16,21 @@
           <template slot="items" slot-scope="props">
               <td><v-checkbox primary hide-details v-model="props.selected"></v-checkbox></td>
               <td align="left">{{props.item.name}}</td>
+
+              <td v-if="props.item.running">
+                    <v-progress-linear v-model="props.item.percent_done"/>
+              <td v-else>
+                  <v-tooltip top>
+                      <v-icon slot="activator" :color="props.item.statusIconColor">{{props.item.statusIcon}}</v-icon>
+                      <span>{{props.item.statusIconTooltip}}</span>
+                  </v-tooltip>
+              </td>
+
+              <td v-if="props.item.running">
+                  <v-icon>cancel</v-icon>
+              </td>
+              <td v-else/>
+
               <td align="left">{{props.item.start_time | dateTime}}</td>
               <td align="left">{{props.item.elapsed_time | timeInterval}}</td>
               <td align="left">{{props.item.errors.length}}</td>
@@ -36,7 +52,9 @@ export default {
     data () {
         return {
             headers: [
-                {text: 'Name', value: 'name', align: 'left'},
+                {text: 'Running', value: 'running', align: 'left'},
+                {text: 'Progress', value: 'running', align: 'left'},
+                {text: 'Cancel', value: 'running', align: 'left'},
                 {text: 'Start Time', value: 'start_time', align: 'left'},
                 {text: 'Elapsed Time', value: 'elapsed_time', align: 'left'},
                 {text: 'Errors', value: 'errors', align: 'left'},
@@ -48,7 +66,7 @@ export default {
             search: '',
             selected: [],
             pagination: {
-                sortBy: 'start_time',
+                sortBy: 'running',
                 descending: true
             }
         }
@@ -63,8 +81,20 @@ export default {
             const response = await Services.getIngestionTasks()
             this.tasks = response.data
 
+            // Add computed properties to help build the UI
             for (var i = 0; i < this.tasks.length; i++) {
-                this.tasks[i].elapsed_time = this.tasks[i].end_time - this.tasks[i].start_time;
+                var endTime = this.tasks[i].running ? new Date().getTime() : this.tasks[i].end_time;
+                this.tasks[i].elapsed_time = endTime - this.tasks[i].start_time;
+                this.tasks[i].percent_done *= 100;
+
+                if (this.tasks[i].failed || this.tasks[i].canceled) {
+                    this.tasks[i].statusIcon = 'error'
+                    this.tasks[i].statusIconColor = 'red'
+                    this.tasks[i].statusIconTooltip = this.tasks[i].failed ? 'failed' : 'canceled'
+                } else {
+                    this.tasks[i].statusIcon = 'done'
+                    this.tasks[i].statusIconTooltip = 'success'
+                }
             }
         }
     }
